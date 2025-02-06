3e7b1edd05389b8cbda53acd92119f0f6968fbea
avinashDevWorks <jainavinash1911@outlook.com>
2025-02-06 20:21:21 +0530

    Add project files.

---
+++ InputDataProcessor/InputProcessingEngine.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputDataProcessor
+{
+    public class InputProcessingEngine
+    {
+		private int numberOfTestCases;
+
+		public int NumberOfTestCases
+		{
+			get { return numberOfTestCases; }
+			set { numberOfTestCases = value; }
+		}
+
+		private int numberOfInputs;
+		
+
+		public int NumberOfInputs
+		{
+			get { return numberOfInputs; }
+			set { numberOfInputs = value; }
+		}
+
+
+	}
+}
