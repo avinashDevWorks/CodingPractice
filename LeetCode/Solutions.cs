bf2a88f871481286329887e6d52974b9f11119a1
avinashDevWorks <jainavinash1911@outlook.com>
2025-02-06 21:05:45 +0530

    Solution - LeetCode - Easy - TwoSome

--- LeetCode/Solutions.cs
+++ LeetCode/Solutions.cs
@@ -8,6 +8,19 @@ namespace LeetCode
 {
     public class Solutions
     {
-
+        public int[] TwoSum(int[] nums, int target)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int complement = target - nums[i];
+                if (dict.ContainsKey(complement))
+                {
+                    return new int[] { dict[complement], i };
+                }
+                dict[nums[i]] = i;
+            }
+            throw new ArgumentException("No two sum solution");
+        }
     }
 }
