82a9d42273ac5b459f751ee2cb92c06d849f0d73
avinashDevWorks <jainavinash1911@outlook.com>
2025-07-05 11:16:54 +0530

    [Leetcode] : 5th July 2025

--- InputDataProcessor/Program.cs
+++ InputDataProcessor/Program.cs
@@ -1,5 +1,6 @@
 ﻿using LeetCode;
 using System;
+using System.Collections.Generic;
 
 namespace InputDataProcessor
 {
@@ -7,11 +8,12 @@ namespace InputDataProcessor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter problem: ");
-            string problemName = Console.ReadLine();
+            Console.WriteLine(" Select Problem : ");
+            Console.WriteLine("[1] : Two Sum \t [2] : Palindrome Number \t");
+            int problemName = Int32.Parse(Console.ReadLine());
             switch (problemName)
             {
-                case "TwoSum":
+                case 1:
                     TwoSomeInputProcessor.ReadInputValuesTwoSum(out int[] values, out int target);
                     int[] result = TwoSomeSolution.TwoSum(values, target);
                     foreach (int i in result)
@@ -20,17 +22,20 @@ namespace InputDataProcessor
                     }
                     break;
 
+                case 2:
+                    List<int> listOfNum = new List<int> { 121, 12223, 24788742, 1, 0, -34567 };
+                    foreach (int num in listOfNum)
+                    {
+                        bool isPalindrome = PalindromeNumber.IsPalindrome(num);
+                        Console.WriteLine($"Is {num} a palindrome? {isPalindrome}");
+                    }
+                    break;
+
                 default:
                     Console.WriteLine( "Problem not solved yet");
                     break;
             }
-
-
             Console.ReadLine();
         }
-
-
-        
-
     }
 }
---
+++ LeetCode/PalindromeNumber.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class PalindromeNumber
+    {
+        //Given an integer x, return true if x is a palindrome, and false otherwise.
+
+        public static bool IsPalindrome(int x)
+        {
+           if(x < 0 ) return false;
+           return x == ReversingNumber(x);
+        }
+
+        private static int ReversingNumber(int n)
+        {
+            int revN = 0;
+            int lastDigit = 0;
+            while (n > 0)
+            {
+                lastDigit = n % 10;
+                n /= 10;
+                revN = (revN * 10) + lastDigit;
+            }
+            return revN;
+        }
+    }
+}
