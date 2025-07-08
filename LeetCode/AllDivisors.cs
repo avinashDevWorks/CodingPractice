236e42288e101c007f088f58f79ef6482d3c4864
avinashDevWorks <jainavinash1911@outlook.com>
2025-07-08 22:48:10 +0530

    More problems

---
+++ LeetCode/AllDivisors.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class AllDivisors
+    {
+
+        public static List<int> PrintAllDivisors(int n)
+        {
+            List<int> divisors = new List<int>();   
+            for (int i= 1; i<= n/2; i++)
+            {
+                if(n % i == 0)
+                {
+                    divisors.Add(i);
+                }
+            }
+            divisors.Add(n); // Adding the number itself as a divisor
+            return divisors;
+        }
+    }
+}
---
+++ LeetCode/LongestNonRepeatingSubstring.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode
+{
+    public class LongestNonRepeatingSubstring
+    {
+        //Given a string s, find the length of the longest substring without duplicate characters.
+        // "abcabcbb"
+        public static int LengthOfLongestSubstring(string s)
+        {
+            var charSet = new HashSet<char>();
+            int left = 0, maxLength = 0;
+
+            for (int right = 0; right < s.Length; right++)
+            {
+                while (charSet.Contains(s[right]))
+                {
+                    charSet.Remove(s[left]);
+                    left++;
+                }
+
+                charSet.Add(s[right]);
+                maxLength = Math.Max(maxLength, right - left + 1);
+            }
+
+            return maxLength;
+        }
+    }
+}
